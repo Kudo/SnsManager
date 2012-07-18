5a03dae2f312f7c8109bc45805af40c861ed6d76
Kudo Chien <ckchien@gmail.com>
2012-07-18 17:39:41 +0800

    Fix test

--- tests/it_FbBase.py
+++ tests/it_FbBase.py
@@ -1,38 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    test_FbUserInfo.py - Integration test for FbUserInfo
+    test_FbBase.py - Integration test for FbBase
 """
 import sys, os.path
 # Hack for import module in grandparent folder
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-from FbUserInfo import FbUserInfo
 import unittest
+from SnsManager.facebook.FbBase import FbBase
 
 TEST_TOKEN = 'AccessToken'
 
-class TestFbUserInfo(unittest.TestCase):
+class TestFbBase(unittest.TestCase):
     def test_GetMyName_EqualMyName_True(self):
-        obj = FbUserInfo(accessToken=TEST_TOKEN)
+        obj = FbBase(accessToken=TEST_TOKEN)
         self.assertEqual(obj.getMyName(), 'Kudo Chien')
 
     def test_GetMyEmail_EqualMyEmail_True(self):
-        obj = FbUserInfo(accessToken=TEST_TOKEN)
+        obj = FbBase(accessToken=TEST_TOKEN)
         self.assertEqual(obj.getMyEmail(), 'ckchien@gmail.com')
 
     def test_GetMyAvatar_HasAvatarUri_True(self):
-        obj = FbUserInfo(accessToken=TEST_TOKEN)
+        obj = FbBase(accessToken=TEST_TOKEN)
         self.assertTrue(obj.getMyAvatar())
 
     def test_IsTokenValid_GivenValidToken_True(self):
-        obj = FbUserInfo(accessToken=TEST_TOKEN)
+        obj = FbBase(accessToken=TEST_TOKEN)
         self.assertTrue(obj.isTokenValid())
 
     def test_IsTokenValid_GivenInValidToken_False(self):
-        obj = FbUserInfo(accessToken='invalid_token')
+        obj = FbBase(accessToken='invalid_token')
         self.assertFalse(obj.isTokenValid())
 
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestFbUserInfo)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFbBase)
     unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ tests/it_FbExporter.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    test_FbExporter.py - Integration test for FbExporter
+"""
+import sys, os.path
+# Hack for import module in grandparent folder
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from SnsManager import ErrorCode
+from SnsManager.facebook.FbExporter import FbExporter
+import unittest
+import logging
+
+TEST_TOKEN = 'AAAFExPZCm0AwBAE5kSkN0srI5B3XpuCYD2n7WKzVeRCewpvcvMH3x7jJ2ZB2ZC48CW9MZCroGx8OAKxaefcAeZB2VMRj2z8sPB9S6dRSwtlQLsp71mGXk'
+
+class TestFbExporter(unittest.TestCase):
+    def test_GetData_ReturnValidFormat_True(self):
+        obj = FbExporter(accessToken=TEST_TOKEN)
+        resp = obj.getData()
+        self.assertIn('retCode', resp)
+        self.assertEqual(resp['retCode'], ErrorCode.S_OK)
+        self.assertTrue(ErrorCode.IS_SUCCEEDED(resp['retCode']))
+        self.assertIn('data', resp)
+        self.assertTrue(type(resp['data']), list)
+        self.assertIn('count', resp)
+        self.assertTrue(type(resp['count']), int)
+
+    def test_GetData_GivenInvalidToken_ReturnInvalidToken(self):
+        obj = FbExporter(accessToken='invalid_token')
+        resp = obj.getData()
+        self.assertIn('retCode', resp)
+        self.assertEqual(resp['retCode'], ErrorCode.E_INVALID_TOKEN)
+
+    def test_IsTokenValid_GivenValidToken_True(self):
+        obj = FbExporter(accessToken=TEST_TOKEN)
+        resp = obj.isTokenValid()
+        self.assertEqual(resp, ErrorCode.S_OK)
+
+    def test_IsTokenValid_GivenInValidToken_False(self):
+        obj = FbExporter(accessToken='invalid_token')
+        resp = obj.isTokenValid()
+        self.assertEqual(resp, ErrorCode.E_INVALID_TOKEN)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFbExporter)
+    unittest.TextTestRunner(verbosity=2).run(suite)
