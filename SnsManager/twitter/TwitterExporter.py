66a54e115fcbcdaaa44bce1eb285b63054ae1b6e
Kudo Chien <ckchien@gmail.com>
2012-12-06 12:02:25 +0800

    Support favorites and retweets

--- SnsManager/twitter/TwitterExporter.py
+++ SnsManager/twitter/TwitterExporter.py
@@ -3,6 +3,8 @@ from TwitterBase import TwitterBase
 from SnsManager import ErrorCode, IExporter
 
 class TwitterExporter(TwitterBase, IExporter):
+    _API_LIST = ['user_timeline', 'favorites', 'retweeted_by_me']
+
     def __init__(self, *args, **kwargs):
         super(TwitterExporter, self).__init__(*args, **kwargs)
 
@@ -12,7 +14,7 @@ class TwitterExporter(TwitterBase, IExporter):
     def getData(self, **kwargs):
         """
         In:
-            lastSyncId *optional*       --  The last synced ID
+            lastSyncId *optional*       --  The last synced ID which is a dict type for different API's ID.
             exportDirection             --  self.EXPORT_DIRECTION_FORWARD or self.EXPORT_DIRECTION_BACKWARD
 
         Out:
@@ -46,32 +48,36 @@ class TwitterExporter(TwitterBase, IExporter):
         if not self.myId:
             return retDict
 
-        retLastSyncId = None
-        if exportDirection == self.EXPORT_DIRECTION_BACKWARD:
-            params = {}
-            if lastSyncId:
-                params['max_id'] = lastSyncId - 1
-            for status in tweepy.Cursor(self._tweepy.user_timeline, **params).items(limit=limit):
-                data = {
-                    'id': status.id,
-                    'message': status.text,
-                    'createdTime': status.created_at
-                }
-                retLastSyncId = status.id
-                retDict['data'][data['id']] = data
-        else:
-            params = {}
-            if lastSyncId:
-                params['since_id'] = lastSyncId
-            for status in tweepy.Cursor(self._tweepy.user_timeline, **params).items():
-                if not retLastSyncId:
-                    retLastSyncId = status.id
-                data = {
-                    'id': status.id,
-                    'message': status.text,
-                    'createdTime': status.created_at
-                }
-                retDict['data'][data['id']] = data
+        retLastSyncId = {}
+        for api in self._API_LIST:
+            retLastSyncId[api] = None
+            if exportDirection == self.EXPORT_DIRECTION_BACKWARD:
+                params = {}
+                if type(lastSyncId) == dict and api in lastSyncId:
+                    params['max_id'] = lastSyncId[api] - 1
+                for status in tweepy.Cursor(getattr(self._tweepy, api), **params).items(limit=limit):
+                    data = {
+                        'id': status.id,
+                        'message': status.text,
+                        'createdTime': status.created_at,
+                        'type': api,
+                    }
+                    retLastSyncId[api] = status.id
+                    retDict['data'][data['id']] = data
+            else:
+                params = {}
+                if type(lastSyncId) == dict and api in lastSyncId:
+                    params['since_id'] = lastSyncId[api]
+                for status in tweepy.Cursor(getattr(self._tweepy, api), **params).items():
+                    if not retLastSyncId[api]:
+                        retLastSyncId[api] = status.id
+                    data = {
+                        'id': status.id,
+                        'message': status.text,
+                        'createdTime': status.created_at,
+                        'type': api,
+                    }
+                    retDict['data'][data['id']] = data
 
         retDict['lastSyncId'] = retLastSyncId
         retDict['count'] = len(retDict['data'])
