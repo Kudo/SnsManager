56b32f0459c8efe5514491aff4daa39b1950cd9c
Kudo Chien <ckchien@gmail.com>
2012-12-13 15:54:45 +0800

    Support refresh token

--- SnsManager/google/GoogleBase.py
+++ SnsManager/google/GoogleBase.py
@@ -1,17 +1,23 @@
 import httplib2
 from apiclient.discovery import build
-from oauth2client.client import AccessTokenCredentials, AccessTokenCredentialsError
+from oauth2client.client import OAuth2Credentials, AccessTokenRefreshError
 from SnsManager.SnsBase import SnsBase
 from SnsManager import ErrorCode
 
 class GoogleBase(SnsBase):
     def __init__(self, *args, **kwargs):
         super(GoogleBase, self).__init__(*args, **kwargs)
+        for k in ['refreshToken', 'clientId', 'clientSecret']:
+            if k not in kwargs:
+                raise ValueError('Invalid parameters.')
+            setattr(self, '_' + k, kwargs[k])
+
         self.myId = self.getMyId()
         self._userAgent = 'Waveface AOStream/1.0'
+        self._tokenUri = 'https://accounts.google.com/o/oauth2/token'
 
         self._http = httplib2.Http()
-        credentials = AccessTokenCredentials(self._accessToken, self._userAgent)
+        credentials = OAuth2Credentials(self._accessToken, self._clientId, self._clientSecret, self._refreshToken, None, self._tokenUri, self._userAgent)
         self._http = credentials.authorize(self._http)
 
     def getMyId(self):
@@ -25,7 +31,7 @@ class GoogleBase(SnsBase):
     def isTokenValid(self):
         try:
             userInfo = build('oauth2', 'v2', http=self._http).userinfo().get().execute()
-        except AccessTokenCredentialsError as e:
+        except AccessTokenRefreshError as e:
             return ErrorCode.E_INVALID_TOKEN
         except:
             self._logger.exception('GoogleBase::isTokenValid() exception')
